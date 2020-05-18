3ef8d4ae16768b125891be147ef1b9fd0197b989
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-18 20:29:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
 def hello_t(array)
-  names.each { |name| puts name }
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
-# call your method here!
-
